d10795007629ed39b59fd92d3291dd9db5084b2e
javi <javi@JJ-HPLAPTOP>
2019-02-03 12:28:16 +0100

    Controller implementation with business methods to get account list for customer and movement list for account.

--- src/clientside/controller/CustomerManagerImplementation.java
+++ src/clientside/controller/CustomerManagerImplementation.java
@@ -10,6 +10,8 @@ import clientside.model.Customer;
 import clientside.model.Movement;
 import clientside.restclient.CustomerRESTClient;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 /**
@@ -17,40 +19,68 @@ import java.util.stream.Collectors;
  * @author javi
  */
 public class CustomerManagerImplementation implements CustomerManager{
-
-    private CustomerRESTClient client;
-    
+    /**
+     * Customer whose info is managed by this manager. It is like a session 
+     * attribute.
+     */
     private Customer customer;
-
-    public CustomerManagerImplementation(){
-        client=new CustomerRESTClient();
-    } 
+    /**
+     * Default URI for server side application.
+     */
+    private String URI="http://hz059359:8080/CRUDBankServerSide/webresources";
+    private static Logger LOGGER=Logger.getLogger("clientside.controller");
+    /**
+     * Get customer full info: personal data, accounts and their movements from 
+     * a customer RESTful service in a Java EE Server.
+     * @param id The customer id.
+     * @return A Customer object containing full info.
+     */
     @Override
     public Customer getCustomerAccountsFullInfo(Long id) {
+        LOGGER.log(Level.INFO,"Getting full info for customer {0}",id.toString());
+        CustomerRESTClient client=new CustomerRESTClient();
+        client.setWebTarget(URI, "customer");
         customer=client.find(Customer.class, id.toString());
+        client.close();
+        if(customer!=null)
+            LOGGER.log(Level.INFO,"Got info for {0}",customer.toString());
         return customer;
     }
-
+    /**
+     * Get accounts list for customer.
+     * @param customer The Customer object.
+     * @return A list of Account objects containing account info.
+     */
     @Override
     public List<Account> getCustomerAccountsList(Customer customer) {
+        if(customer!=null)
+            LOGGER.log(Level.INFO,"Getting accounts for customer {0}",customer.toString());
         if(this.customer==null || !this.customer.equals(customer))
-            this.customer=client.find(Customer.class, customer.getId().toString());
+            this.customer=getCustomerAccountsFullInfo(customer.getId());
         return this.customer.getAccounts();
     }
-
-    @Override
+    /**
+     * Get movements list for customer.
+     * @param account
+     * @return A list of Movement objects containing movements info.
+     */
+     @Override
     public List<Movement> getAccountMovementsList(Account account) {
-        if(this.customer==null || !this.customer.equals(customer))
-            this.customer=client.find(Customer.class, customer.getId().toString());
-        return this.customer.getAccounts().stream()
+        if(account!=null)
+            LOGGER.log(Level.INFO,"Getting movements for account {0}",account.toString());
+        List<Movement> movements=this.customer.getAccounts().stream()
                 .filter(it->it.equals(account))
                 .collect(Collectors.toList()).get(0).getMovements();
+        return movements;
      }
-
+    /**
+     * Set server name for serverside application to be used 
+     * @param serverName the serverName in which the server side application resides.
+     */
     @Override
     public void setServerName(String serverName) {
-        String URI = "http://"+serverName+":8080/CRUDBankServerSide/webresources";
-        client.setWebTarget(URI, "customer");
+        if(serverName!=null)
+            LOGGER.log(Level.INFO,"Setting server name to {0}",serverName);
+        URI = "http://"+serverName+":8080/CRUDBankServerSide/webresources";
     }
-    
 }
--- src/clientside/restclient/CustomerRESTClient.java
+++ src/clientside/restclient/CustomerRESTClient.java
@@ -27,13 +27,15 @@ public class CustomerRESTClient {
     private WebTarget webTarget;
     private Client client;
     private static final String BASE_URI = "http://localhost:8080/CRUDBankServerSide/webresources";
-
+    /**
+     * Construct a JAX RS client for default localhost server and customer service.
+     */
     public CustomerRESTClient() {
         client = javax.ws.rs.client.ClientBuilder.newClient();
         webTarget = client.target(BASE_URI).path("customer");
     }
     /**
-     * Sets the target for the RESTful client.
+     * Set the target for the RESTful client.
      * @param base_uri URI value for the RESTful service to be invoked. It does not
      * include path identifying the service. See next parameter.
      * @param path Path value added to base_uri to select RESTful service to be used.
@@ -41,42 +43,82 @@ public class CustomerRESTClient {
     public void setWebTarget(String base_uri,String path){
         webTarget = client.target(base_uri).path(path);
     }
-
+    /**
+     * Get customers count. 
+     * @return The customers count.
+     * @throws ClientErrorException HTTP error.
+     */
     public String countREST() throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path("count");
         return resource.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(String.class);
     }
-
+    /**
+     * Update customer data.
+     * @param requestEntity Customer instance containing data. 
+     * @param id Customer id.
+     * @throws ClientErrorException HTTP error.
+     */
     public void edit(Object requestEntity, String id) throws ClientErrorException {
         webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id})).request(javax.ws.rs.core.MediaType.APPLICATION_XML).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
-
+    /**
+     * Get customer data.
+     * @param <T> Customer.
+     * @param responseType Customer class.
+     * @param id Customer id.
+     * @return Customer.
+     * @throws ClientErrorException HTTP error.
+     */
     public <T> T find(Class<T> responseType, String id) throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{id}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
-
+    /**
+     * Get a range of customers (useful for management of big data volume). 
+     * @param <T> type of the collection of customers.
+     * @param responseType class for the type of the collection of customers.
+     * @param from initial index value for query.
+     * @param to final index value for query.
+     * @return A collection of Customers.
+     * @throws ClientErrorException HTTP error.
+     */
     public <T> T findRange(Class<T> responseType, String from, String to) throws ClientErrorException {
         WebTarget resource = webTarget;
         resource = resource.path(java.text.MessageFormat.format("{0}/{1}", new Object[]{from, to}));
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
-
+    /**
+     * Create a Customer on server side.
+     * @param requestEntity Customer instance to be created.
+     * @throws ClientErrorException HTTP error.
+     */
     public void create(Object requestEntity) throws ClientErrorException {
         webTarget.request(javax.ws.rs.core.MediaType.APPLICATION_XML).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.APPLICATION_XML));
     }
-
+    /**
+     * Get all Customers data.
+     * @param <T> type of the collection of customers.
+     * @param responseType class of the type of the collection of customers.
+     * @return A collection of Customers.
+     * @throws ClientErrorException HTTP error.
+     */
     public <T> T findAll(Class<T> responseType) throws ClientErrorException {
         WebTarget resource = webTarget;
         return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
     }
-
+    /**
+     * Delete a Customer on server side.
+     * @param id id of the Customer to be deleted.
+     * @throws ClientErrorException HTTP error.
+     */
     public void remove(String id) throws ClientErrorException {
         webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{id})).request().delete();
     }
-
+    /**
+     * Close client.
+     */
     public void close() {
         client.close();
     }
